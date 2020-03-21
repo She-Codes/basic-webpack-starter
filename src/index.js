fe255cf3767f2c77d85696bfb95711eca833f558
She-Codes <n.y.shaw@gmail.com>
2020-03-21 19:48:56 -0400

    added a package to practice importing from node_modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './styles/style.scss';
 import months from './js/doathing';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
 console.log('whut');
 console.log(months());
+tippy('[data-tippy-content]');
