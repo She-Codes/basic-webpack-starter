31add665818d216d0514d0746aff52b71833a08c
She-Codes <n.y.shaw@gmail.com>
2020-03-21 20:16:19 -0400

    added fetch request to test functionality
    CORS error but seems to be working.  Had to edit babel config due to 'regeneratorRuntime is not defined' error from webpack

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import months from './js/doathing';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
+const baseUrl =
+  'http://api.openweathermap.org/data/2.5/weather';
+const apiKey = '7ac846b1627ac681186c50292ce4d755';
+
 console.log('whut');
 console.log(months());
 tippy('[data-tippy-content]');
+
+const fetchWeatherData = async () => {
+  const data = await fetch(
+    `${baseUrl}?zip=23225&units=imperial&appid=${apiKey}`
+  );
+  console.log(data);
+};
+
+fetchWeatherData();
